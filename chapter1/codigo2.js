f4f6d860c0c03cfa132186a4c6f3fc6c395f8e51
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-08-30 11:46:59 -0500

    initial commit

---
+++ chapter1/codigo2.js
@@ -0,0 +1,37 @@
+//1.45pm
+//alert('codigo2');  
+//num1 = 5;
+//num1++;
+//num1 = -num1 //neg sign to turn it into a neg number 
+//document.write(num1);
+
+//  CONCAT //
+saludo = 'hola paco, '; 
+pregunta = 'como estas?'; 
+frase = saludo + pregunta; 
+document.write (frase); 
+
+nombre = 'paco'; 
+frase = `si, hola. yo soy ${nombre} y estoy aqui, estudiando`
+
+document.write (frase); 
+
+numero = 25; 
+numero2 = 36; 
+numer = 25; 
+num = 'twentyfive';
+document.write (numero == numero2); 
+document.write (numero == numer); 
+document.write(numero == num);
+document.write( numero === numer);
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter2/ch2_helado.js
@@ -0,0 +1,106 @@
+
+/*datos = 'string, number, boolean, undefined, null, nan, ';
+alert(datos);
+
+string = "cadena de texto"; 
+number = 19; 
+booleano = false 
+
+let numero 
+numero = 5
+alert(numero)
+
+let otracosa = 10
+alert(numero + otracosa)
+*/
+/*
+Math.PI
+let diameter = prompt('please enter circles diameter')
+const area = (Math.pow(diameter, 2)) * Math.PI
+alert (area)
+*/
+/*
+//to get 25 you have to comment out //num5--; and //alert (resultado); in alert num3 
+let num5= 5 
+let num10 = 10
+//num5--;
+resultado = num5 + num10; 
+//alert (resultado); 
+num3 = num5**2
+alert(num3)
+//for increments ++, **, -- and more you have to assign it and the show it for value to work
+num3++ 
+alert(num3)
+*/
+//remainder is % in dalton tutorial he used & with numbers 10&5 = 0 which in mozzilla is also 0
+//in mozzila & is The bitwise AND operator (&) returns a 1 in each bit position for which the corresponding /////bits of both operands are 1s (unos). they test it with 14&9 and result is 8. 
+
+/*numero1 = 14
+numero2 = 9
+numero3 = 10        //binary value 0000000000001010
+numero4 = 5         //             0000000000000101    there is no matching 1s between 10&5 so = 0 
+leftover = numero1&numero2;
+resultado = numero1%numero2; 
+dalton = numero3&numero4; 
+alert(leftover)
+alert(resultado)
+alert(dalton)
+*/
+/*let con = '53' 
+let cat = 8
+let frase = con.concat(cat)
+document.write(frase)
+*/
+/*
+let frase = 'We cross our bridges when we come to them and burn them behind us, '
+let frase2 = ' with nothing to show for our progress except a memory of the smell of smoke, '
+let frase3 = 'and a presumption that once our eyes watered. '
+let author = 'Tom Stoppard'
+//prints the same, phrase and author
+document.write (frase+frase2+frase3+'Writen by' + author)
+document.write(`${frase} ${frase2} ${frase3} Writen by ${author}`)
+
+//let verdad = prompt('please enter a numero')
+//let falso = prompt('please enter another numero')
+let num = prompt('please enter a numero')
+let txt = prompt('please write a numero')
+//alert(verdad == falso);
+*/
+/*if (verdad == falso){
+    alert(`nums entered, ${verdad} and ${falso} are equalE`)
+}
+if (verdad != falso) {
+    alert(`nums entered, ${verdad} and ${falso} are NOT equalE`)
+}
+if (verdad === falso) {
+    alert ( `${verdad} and ${falso} are StrictlY equalE`)
+}
+*/
+/*
+if (num === txt) {
+    alert (`${num} and ${txt} arE strictly the SAME`)
+}
+if (num == txt){
+    alert (`${num} and ${txt} are NOT StrictlY eQualE`)
+}
+if (num !==  txt) {
+    alert (`${num} and ${txt} are differente`)
+}
+*/
+
+
+//STRICTLY ===,  !== are important, they do make a difference. from the normal !=, ==
+
+
+
+let valor = true
+let balor = true
+let resultado = (valor && balor) // true
+let resultado2 = (valor || balor)
+let resultado = (valor )
+
+
+
+
+
+
