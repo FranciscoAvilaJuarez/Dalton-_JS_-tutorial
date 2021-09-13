c7cf17af8dc515672e81e70450de85c1657047f5
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-13 12:13:45 -0500

    toy problem ch3

--- CLASES/getSet.js
+++ CLASES/getSet.js
@@ -2,12 +2,12 @@
 
 
 class ANIMAL {      //open ANIMALC
-    constructor(nombre, especie, edad, color){      
+    constructor(nombre, ezpecie, edad, color){      
         this.name = nombre; 
-        this.especie = especie;     //SET changed to especie in white, 'raza' was NOT working
+        this.ezpecie = ezpecie;     //SET changed to ezpecie in white, 'raza' was NOT working
         this.edad = parseInt(edad);    
         this.color = color;
-        this.info = `me llamo ${this.name} soy ${this.especie} con ${this.edad}yrs de color ${this.color}<br>`; 
+        this.info = `me llamo ${this.name} soy ${this.ezpecie} con ${this.edad}yrs de color ${this.color}<br>`; 
     }
     verInfo(){      //open METODO to see info 
         document.write(this.info + "<br>");
@@ -15,14 +15,17 @@ class ANIMAL {      //open ANIMALC
 }       //close ANIMALC
 
 
-class PERRO extends ANIMAL {        //open PC E A
-    constructor( nombre, especie, edad, color, size ){        //open cons       STEP2 ADD IT
-        super( nombre, especie, edad, color, size );            //STEP3 PASS IT 
-        this.especie = null;        //again pass especie but null it to change it       STEP3 NULL THE CURRENT
+class PERRO extends ANIMAL {       
+    constructor( nombre, ezpecie, edad, color, size ){        //open cons       STEP2 ADD IT
+        super( nombre, ezpecie, edad, color, size );            //STEP3 PASS IT 
+        this.ezpecie = null;        //again pass ezpecie but null it to change it       STEP3 NULL THE CURRENT
     }//close cons
-    set changeEspecie(newEspecie){      //added the SET to make changes     //STEP4 ADD SET 'METHOD'
-        this.especie = newEspecie;          //made change       //STEP5 ASSIGN NEW VALUE FROM STEP1 
-    }//ladrarF
+    set setEzpecie(newEzpecie){      //added the SET to make changes     //STEP4 ADD SET 'METHOD'
+        this.ezpecie = newEzpecie;          //made change       //STEP5 ASSIGN NEW VALUE FROM STEP1 
+    }//close SET
+    get getEzpecie (){
+        return this.ezpecie; 
+    }
 }//close PC E A
 
 //enters values. 
@@ -31,9 +34,12 @@ const gato = new ANIMAL( 'garfield', 'gato', 5, 'stripped meow orange' );
 const bird = new ANIMAL( 'heloo', 'bird', 3, 'green' ); 
 
 
-perro.changeEspecie = 'lobo'        //step one, make change to 'lobo' FROM perro (s1 for changes to take place)
-document.write(perro.especie)       //STEP6 PRINT OUT NEW VALUE lobo
-
+perro.setEzpecie = 'lobo';     //step one, make change to 'lobo' FROM perro (s1 for changes to take place)
+document.write(perro.ezpecie + '<br>');        //STEP6 PRINT OUT NEW VALUE lobo
+document.write(perro.getEzpecie); 
+document.write(perro.ezpecie + '<br>');
+console.log(perro.getEzpecie); 
+console.log(perro.newEzpecie); 
 /*
 
 //  probing static, always running'on' 
