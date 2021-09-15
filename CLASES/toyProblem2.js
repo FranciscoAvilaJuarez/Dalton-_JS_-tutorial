6f50f5ec4d6ffe30d011a03a3f90858559caf995
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-15 18:06:00 -0500

    IF/validate

--- CLASES/toyProblem2.js
+++ CLASES/toyProblem2.js
@@ -6,7 +6,53 @@
 */
 
 class App {//open App-class 
-    constructor(){//open conS
+    constructor(descargas, puntuacion, peso){//open conS
+        this.descargas = descargas; 
+        this.puntuacion = puntuacion; 
+        this.peso = peso; 
 
+        this.iniciada = false; 
+        this.instalada = false; 
     }//close cons
-}//close App-class
+
+    //abrir method 
+    abrir(){
+        if (this.iniciada == false){
+            this.iniciada = true; 
+            if ( this.iniciada == true){//validate is true after changing to true
+                alert(`${this.app} iniciada`); 
+            }
+        }
+    };//close abrir
+
+    //cerrar
+    cerrar(){
+        if(this.iniciada == true){
+            this.iniciada == false; 
+            if( this.iniciada == false){//validate is false after changing to false
+                alert(`${this.app} apagada`); 
+            };
+        };
+    };//close cerrar
+
+    //instalar
+    instalar(){
+        if(this.instalada == false){
+            this.instalada = true; 
+            if ( this.instalada == true ){//validate is true after changing to true
+                alert ( `${this.app} esta instalada`)
+            }
+        }
+    };//close instalar
+
+    //desinstalar
+    desinstalar(){
+        if(this.instalada == true){
+            this.instalada == false; 
+            if ( this.instalada == false ){//validate is false after changing to false
+                alert(`${this.app} esta desInstalada`)
+            }
+        }
+    };//close desinstalar
+
+};//close App-class
