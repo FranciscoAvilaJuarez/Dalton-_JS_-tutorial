e15ed88d23c13c06f4ab62abfee622daca68b52b
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-15 18:50:26 -0500

    show info

--- CLASES/toyProblem2.js
+++ CLASES/toyProblem2.js
@@ -5,54 +5,62 @@
 *have to be able to install/unistall and open/close
 */
 
-class App {//open App-class 
-    constructor(descargas, puntuacion, peso){//open conS
-        this.descargas = descargas; 
-        this.puntuacion = puntuacion; 
-        this.peso = peso; 
+class Applicacion {//open App-class 
+    constructor(nombre,descargas, puntuacion, peso){//open conS
+        this.nombre = nombre;
+        this.descargas = descargas + ' usuarios'; 
+        this.puntuacion = puntuacion + ' estrellas'; 
+        this.peso = peso + 'Kb'; 
 
         this.iniciada = false; 
         this.instalada = false; 
     }//close cons
 
-    //abrir method 
-    abrir(){
-        if (this.iniciada == false){
-            this.iniciada = true; 
-            if ( this.iniciada == true){//validate is true after changing to true
-                alert(`${this.app} iniciada`); 
-            }
-        }
-    };//close abrir
-
-    //cerrar
-    cerrar(){
-        if(this.iniciada == true){
-            this.iniciada == false; 
-            if( this.iniciada == false){//validate is false after changing to false
-                alert(`${this.app} apagada`); 
-            };
-        };
-    };//close cerrar
-
     //instalar
     instalar(){
         if(this.instalada == false){
             this.instalada = true; 
-            if ( this.instalada == true ){//validate is true after changing to true
-                alert ( `${this.app} esta instalada`)
-            }
-        }
+            alert ( `${this.nombre} 1 instalar-esta instalada`)
+        }//close if
     };//close instalar
+
+    //abrir method 
+    abrir(){
+        if (this.iniciada == false && this.instalada == true ){
+            this.iniciada = true; 
+                alert(`${this.nombre} 2 abrir-esta abierta`); 
+        }//close if 
+    };//close abrir
+
+    //cerrar
+    cerrar(){
+        if( this.iniciada == true && this.instalada == true ){
+            this.iniciada == false; 
+            alert(`${this.nombre} 3 cerrar-esta cerrada`); 
+        }//close if
+    }//close cerrar
 
     //desinstalar
     desinstalar(){
         if(this.instalada == true){
             this.instalada == false; 
-            if ( this.instalada == false ){//validate is false after changing to false
-                alert(`${this.app} esta desInstalada`)
-            }
-        }
+            alert(`${this.nombre} 4 desinstalar-esta desInstalada`)
+        }//close if
     };//close desinstalar
-
 };//close App-class
+
+
+//      nombre,descargas, puntuacion, peso
+let app = new Applicacion('istagram', '90', '8', '950' ); 
+
+
+//call app
+app.instalar();
+app.abrir();
+ app.cerrar();
+app.desinstalar();  
+
+
+
+
+
