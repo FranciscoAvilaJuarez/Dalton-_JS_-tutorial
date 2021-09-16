e7df425b8690055c2e08ba7725667fac8da1739e
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-15 21:42:51 -0500

    chain methods

---
+++ Chapter4/chainMethods.js
@@ -0,0 +1,13 @@
+/*chain methods
+    //ernesto
+    //se puede usar para cambiar la primera 'e' por 'E' en ernesto
+
+    metodo, cadena que busca ernesto con 'ernesto' y la cambia por 'Ernesto'
+    y usar 'cadena' para poner un (punto)'.' al final  
+    
+    pero 
+
+    para asegurase 'Ernesto' sigue siendo igual que 'ernesto'
+    con otro metodo que nos dice si/no 
+
+*/    
