21869468973713909b462c9a08ee76ab4de8605c
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-16 00:42:40 -0500

    reboot

--- Chapter4/starts_endsWith.js
+++ Chapter4/starts_endsWith.js
@@ -1,9 +1,28 @@
 //*starts/endsWith()
+//declare em
+let cadena = 'c(1) hola';//if SAPACE MATTERS FROM ('(1) hola,_') TO (' (1) hola,_') THE 'SPACE' BEHIND ' (1)' MAKES IT true
+//let cadena1 = 'ccomo estas' //other example ('como estas') to (' como estas') of 'space behind'
+//let cadena2 = 'c(2) tu';//no space
+//let cadena2_1 = ' (2) tu';//with  space
+//let cadena3 = 'c(3) pater'
 
-let cadena = '(1) hola,_'; 
-let cadena1 = 'como estas_'
-let cadena2 = '(2) tu';
-let cadena3 = '(3) pater'
+/*
+//      declare startsWithS
+let resultadoStart = cadena.startsWith(cadena); // 'cadena' igual to 'cadena'
+let resultadoStart2 = cadena.startsWith(cadena2); //so 'cadena' NOT to igual 'cadena2'
+let resultadoStart2_1 = cadena.startsWith(cadena2_1); // 'cadena' not igual to 'cadena2_1'
 
-let resultado = cadena.startsWith(cadena2); 
-document.write (resultado + '<br>');  
+//      show startsWithS 
+document.write (resultadoStart + '<br>');//shows 'FALSE'  
+document.write (resultadoStart2 + '<br>');//shows 'true'  
+//example of space behind  NOTE : SAW NO CHANGE IN SPACE
+document.write (resultadoStart2_1 + '<br>');//shows 'FALSE' 
+
+*/
+//      declare endsWithS
+searchyEnd = 'a'; 
+let resultado = cadena.endsWith(searchyEnd); 
+
+
+//      show endsWithS
+document.write = (resultado); 
