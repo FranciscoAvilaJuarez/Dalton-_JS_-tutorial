2250a9139dc465327adb44ffac0064352a867f47
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-17 00:33:41 -0500

    chain method repeat()

--- Chapter4/padStart.js
+++ Chapter4/padStart.js
@@ -1,3 +1,4 @@
+document.write('JS test, padStart;' + '<br>'+'<br>'+'<br>'); 
 /*              chain method padStart()
 *padStart()
 *padEnd()
@@ -5,5 +6,27 @@
 
 */
 
-//              padStart()
-document.write('JS test, padStart'); 
+//              padStart() rellena la cadena al principio
+let chain = '-abc'; //has 4 characters
+let chain2 = '_abc'; 
+
+//we're telling to 'fill' chain with 12 characters of '-abc'
+let resultStart = chain.padStart(12, chain2);
+
+//what shows is: '  '_abc_abc-abc' IS the RESULT of 12 '_abc' padStartS inserted into '-abc'  '
+document.write (`
+'${resultStart}' IS the RESULT of 12 '${chain2}' <b>padStartS</b> inserted into '${chain}'
+<br>`);
+
+
+
+//              padEnd()        rellena la cadena al final 
+// insert 12 '_abc' into '-abc' until you have 12characters in '-abc'
+let resultEnd = chain.padEnd(12, chain2);
+
+//what shows is: '  '-abc_abc_abc' IS the RESULT of 12 '_abc' padEndS_ inserted into '-abc'  '
+document.write (`'${resultEnd}' IS the RESULT of 12 '${chain2}' <b> padEndS_</b> inserted into '${chain}'<br>` ); 
+
+
+
+//              repeat() 
