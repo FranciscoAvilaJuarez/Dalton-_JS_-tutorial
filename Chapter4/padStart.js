05b8b8a6543ad1a18073ca215e61ac6f1a852e69
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-17 01:01:45 -0500

    chain method repeat()

--- Chapter4/padStart.js
+++ Chapter4/padStart.js
@@ -15,7 +15,7 @@ let resultStart = chain.padStart(12, chain2);
 
 //what shows is: '  '_abc_abc-abc' IS the RESULT of 12 '_abc' padStartS inserted into '-abc'  '
 document.write (`
-'${resultStart}' IS the RESULT of 12 '${chain2}' <b>padStartS</b> inserted into '${chain}'
+'<b>${resultStart}</b>' IS the RESULT of 12 '${chain2}' <b>'padStartS'</b> inserted into '${chain}'
 <br>`);
 
 
@@ -25,8 +25,29 @@ document.write (`
 let resultEnd = chain.padEnd(12, chain2);
 
 //what shows is: '  '-abc_abc_abc' IS the RESULT of 12 '_abc' padEndS_ inserted into '-abc'  '
-document.write (`'${resultEnd}' IS the RESULT of 12 '${chain2}' <b> padEndS_</b> inserted into '${chain}'<br>` ); 
+document.write (`'<b>${resultEnd}</b>' IS the RESULT of 12 '${chain2}' <b>'padEndS_'</b> inserted into '${chain}'<br>` ); 
+
+//ejemplo en numbers 
+
+let numero =  '_10_';
+let colchonInicio = '0123456789';
+
+//we pass the ammount of characters and what is filling it at the start
+let colchonInicioNumero = numero.padStart(14, colchonInicio ); 
+document.write(`
+we add ${colchonInicio} to ${numero} using 14 spaces with <b>'padStart' : '${colchonInicioNumero}</b>' is NOT a 10 is a one and a zero using two spaces (added '_' to show difference) 
+<br>`); 
+
+
+let colchonFIn = '9876543210'
+//we pass the ammount of characters and what is filling it at the end
+let colchonFinNumero = numero.padEnd(14, colchonFIn);
+//what shows is: we add 9876543210 to _10_ using 14 spaces with 'padEnd' : '_10_9876543210' 
+document.write(`
+we add ${colchonFIn} to ${numero} using 14 spaces with <b>'padEnd' : '${colchonFinNumero}</b>' is NOT a 10 is a one and a zero using two spaces (added '_' to show difference) 
+<br>`); 
 
 
 
 //              repeat() 
+
