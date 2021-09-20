58f2e2837c154d33c8dc7d87f0a7a99426336111
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-20 18:19:47 -0500

    math Object : sqrt()

--- Chapter4/mathObjects.js
+++ Chapter4/mathObjects.js
@@ -4,3 +4,7 @@
                                                                                 //  style='color:#FF0000'
 //math Objects 
 document.write(`<b style='color:#FF0000'> math Objects </b>`);
+
+
+//  sqrt()
+document.write(`'${match.sqrt(25)}'`);
