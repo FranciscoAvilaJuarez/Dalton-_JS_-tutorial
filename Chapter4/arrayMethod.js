aac74c6c6473c39158f9ede56d24a2c864e6dca1
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-20 10:59:36 -0500

    splice()

--- Chapter4/arrayMethod.js
+++ Chapter4/arrayMethod.js
@@ -16,10 +16,11 @@ document.write(`<b>new array 'value'</b> : '${arraiPop}'<br><br><br>`);//pop() f
 //              *push()             *add at end of array
 document.write(`<b style='color:#FF0000'>-*push() *adds at end of array</b><br>`);
 let arraiPush = ['pater', 'comunitaris', 'paco'];
-document.write(`initial array : <b>'${arraiPush}'</b><br>`); 
+document.write(`initial array : '${arraiPush}'<br>`); 
 let arraiPushResult = arraiPush.push('<b>francisco</b>','<b>frankie</b>');//shows five as three in array plus two we are adding
-document.write(`elemento added to position : <b style='color:#FF0000'>'${arraiPushResult}'</b> con <b>push()</b><br>`); 
-document.write(`<b>new array</b> : '${arraiPush}'<br><br><br>`);//new array : 'pater,comunitaris,paco,francisco,frankie'
+document.write(`elemento added con <b>push()</b> : '${arraiPushResult}' <br>`); 
+document.write(`<b>new array</b> : '${arraiPush}'<br>`);//new array : 'pater,comunitaris,paco,francisco,frankie'
+document.write(`'${arraiPush[4]}' is array '${arraiPushResult}' from '${arraiPush.length}' <br><br><br>`);
 
 
 //              *shift()              *eliminates the first one
@@ -27,7 +28,7 @@ document.write(`<b style='color:#FF0000'>-*shift()  *eliminates the first one</b
 let arraiShift = ['<b>pater</b>', 'comunitaris', 'paco'];
 document.write(`initial array : '${arraiShift}' <br>`)
 let arraiShiftResultado = arraiShift.shift();//shift pater out
-document.write(`elemento removido : '${arraiShiftResultado}' with <b>shift()</b><br>`)
+document.write(`elemento removido with <b>shift()</b> : '${arraiShiftResultado}'<br>`)
 document.write(`<b>new array</b> : '${arraiShift}'<br><br><br>`);
 
 
@@ -37,7 +38,7 @@ let arraiUnshift = ['pater', 'comunitaris', 'paco'];
 document.write(`initial array : '${arraiUnshift}'<br>`); 
 document.write(`ammount of elements : <b style='color:#FF0000'>'${arraiUnshift.length}'</b><br>`); 
 let arraiUnshiftResult = arraiUnshift.unshift('<b>frankie</b>', '<b>francisco</b>'); 
-document.write(`ammount of elements : <b style='color:#FF0000'>'${arraiUnshiftResult}'</b> after <b>unshift()</b><br>`);
+document.write(`ammount of elements after using <b>unshift()</b> : <b style='color:#FF0000'>'${arraiUnshiftResult}'</b> <br>`);
 document.write(`<b>new array</b> : '${arraiUnshift}'<br><br><br>`); 
 
 
@@ -47,18 +48,27 @@ let arraiReverse =  ['pater', 'comunitaris', 'paco', 'francisco','<b>frankie</b>
 document.write(`<b>*initial array</b> : '${arraiReverse}'<br>`)//'pater,comunitaris,paco,francisco,frankie'
 let arraiReverseResult = arraiReverse.reverse(); 
 //reverse order : 'frankie,francisco,paco,comunitaris,pater'
-document.write(`*reverse order : '${arraiReverseResult}' con <b>reverse()</b><br>`); 
+document.write(`*after using <b>reverse()</b> : '${arraiReverseResult}'<br>`); 
 //reverse order : 'frankie,francisco,paco,comunitaris,pater'
 document.write(`<b>*new array</b> : '${arraiReverse}'<br><br><br>`);//'frankie,francisco,paco,comunitaris,pater'
 
 
 //              *sort() *organizes alphabetically?
-document.write(`<b style='color:#FF0000'>-*sort() *organizes alphabetically?</b><br>`); 
+document.write(`<b style='color:#FF0000'>-*sort()  __*sorts alphabetically :</b><br>`); 
+let arraiSort = ['z', 'pater', 'comunitaris', 'paco', '<b>avila</b>']; 
+document.write(`initial array : '${arraiSort}'<br>`);
+let arraiSortResult = arraiSort.sort(); 
+document.write(`after using <b>sort()</b> : '${arraiSortResult}' <br>`);
+document.write(`*new array : '${arraiSortResult}'<br><br><br>`);
 
 
+//              *splice() *changes content by deleting or adding
+document.write(`<b style='color:#FF0000'>-*splice() *changes array by delete/adding with (start/end)</b><br>`);
 
-document.write(`<b style='color:#FF0000'>-*splice() *changes content by deleting or adding</b><br>`); 
-
-
+let arraiSplice = ['frankie', 'pater', 'comunitaris', 'avila', 'paco', '<b>frank</b>'];
+document.write(`<b>initial array</b> : '${arraiSplice}'<br>`);
+let arraiSpliceResult = arraiSplice.splice(1,3, 'start', 'end'); 
+document.write(`after using <b>splice(1,3)</b> : '${arraiSpliceResult}' note: <b>frankie & frank</b> are gone <br>`);
+document.write(`<b>new array</b> : '${arraiSpliceResult}'<br>`)
 
 //document.write(``)
