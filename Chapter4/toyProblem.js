2c20dcd7a3e3977b382c3e29d5e67d05276b17db
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-21 17:45:53 -0500

    calculator part two end

--- Chapter4/toyProblem.js
+++ Chapter4/toyProblem.js
@@ -1,77 +1,96 @@
 //add new functions to calculator
 //potencias, raices cuadradas y cubicas
 
-//class summar 
-class sumar {
-    constructor (num1, num2){
-        let sum = parseInt(num1) + parseInt(num2);    
-        return sum; 
-    }
-}
-sum = new sumar();      //initialize class with 'new'
+class Calculadora {
+        constructor(){
+        };//close constructor
+    //summar 
+    sumar(num1, num2) {
+        return parseInt(num1) + parseInt(num2);    
+    };
+    //resta
+    substract(num1, num2) {
+        return parseInt(num1) - parseInt(num2); 
+    }; 
+    //dividir
+    divicion(num1, num2) {
+        return parseInt(num1) / parseInt(num2); 
+    }; 
+    //multiplicacion
+    multiplicar(num1, num2){
+        return parseInt(num1) * parseInt(num2); 
+    };
+    //exp 
+    exponent(num1, expNum){
+        return parseInt(num1) **parseInt(expNum);
+    };
+    //raiz cuadrada
+    raizCuadrada (num1){
+        return parseInt(Math.sqrt(num1));
+    };
+    raizCubica (num1){
+        return parseInt(Math.cbrt(num1)); 
+    };
+};//close class 
 
-//class resta
-class substract {
-    constructor(num1, num2){
-        let resta = parseInt(num1) - parseInt(num2); 
-        return resta; 
-    }
-}
-resta = new substract();        //to call class we have to use 'new' to initialize it
 
-//class dividir
-class divicion {
-    constructor(num1, num2){
-        let divide = parseInt(num1) / parseInt(num2) 
-        return divide;
-    }
-}
-divide = new divicion();        //to call class divicion
+//call class 
+const calculadora = new Calculadora(); 
 
 
 //preguntar que vamos a hacer 
-let operacion = prompt(`que operacion, hoy? 1: suma, 2: resta, 3: divide, 4: multiplica`)
+let operacion = prompt(`
+que operacion, hoy? 
+ 1 : suma, 2 : resta, 3 : divide, 4 : multiplica, 5 : exponencia, 6 : raiz caudrada, 7 : raizCubica
+`);
+
+let num1 = prompt(`enter number to perform '${operacion}' : `);//get first number
+
 // if going to add 
 if (operacion == 1){
-    
-    //llamando clase sumar
-    sum = new sumar(); 
-    
-    //display resultado de suma
-    alert (` el resultado de tu operacion, ${num1} + ${num2} es igual a ${sum}`);
+    let num2 = prompt('second number to sumar'); 
+    sumaResultado = calculadora.sumar(num1, num2);//sumar by calling class 
+    alert(`la suma entre '${num1}' y '${num2}' es : '${sumaResultado}'`)
 }
 //if going to restar
 else if (operacion == 2) {
-    let num1 = prompt('first number to restar'); 
     let num2 = prompt('second number to restar'); 
-    
-    resta = (num1, num2); 
-    //resultado = substract(num1, num2); 
-    alert (` el resultado de tu operacion, ${parseInt(num1)} - ${parseInt(num2)} es igual a ${resta}`);
+    restaResultado = calculadora.substract(num1, num2);//restar using class 
+    alert (`la resta entre : '${num1}' y : '${num2}' es igual a : '${restaResultado}'`);
 }
 //if going to divide
 else if (operacion == 3) {
-    let num1 = prompt('first number to conquer'); 
     let num2 = prompt('second number to conquer'); 
-    
-    divide = (num1, num2); 
-    //resultado = divide(num1, num2); 
-    alert (` el resultado de tu operacion, ${parseInt(num1)} / ${parseInt(num2)} es igual a ${divide}`);
+    divideResultado = calculadora.divicion(num1, num2);//dividir using class 
+    alert (` la divicion entre : '${num1}' y : '${num2}' es igual a : '${divideResultado}'`);
 }
 //if going to multiply
-//multiplicar object; 
 else if (operacion == 4 ){
-    let num1 = prompt('first number to multiply');
     let num2 = prompt('second number to multiply'); 
-
-    object.multiplicar = parseInt(num1) * parseInt(num2);
-    alert (object.multiplicar)
+    multiplyResult = calculadora.multiplicar(num1, num2);//multiply using class
+    alert (`la multiplicacion entre : '${num1}' y : '${num2}' es : '${multiplyResult}'`)
+}
+//exponenciacion
+else if (operacion == 5){
+    let expNum = prompt('a que potencia?'); 
+    let expResult = calculadora.exponent(num1, expNum); 
+    alert(`'${num1}' a la '${expNum}' potencia es : '${expResult}'`);
+}
+//raices cuadradas
+else if (operacion == 6){
+    num1Resultado = calculadora.raizCuadrada(num1);
+    alert(`la raiz caudrada de '${num1}' es : '${num1Resultado}'`);
+}
+//raices Cubicas
+else if (operacion == 7){
+    num1Resultado = calculadora.raizCubica(num1); 
+    alert(`la raiz cubica de '${num1}' es : '${num1Resultado}'`);
 }
 //ANYTHING ELSE
 else {
-    alert('was not able to find opt, ponchaos'); 
+    alert('was not able to find operation'); 
 }
-
+  
 
 
 
