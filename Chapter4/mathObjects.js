e9677ba1c64508daea6d55ec1eedc0d0c5240b57
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2021-09-20 23:25:07 -0500

    math propiedades

--- Chapter4/mathObjects.js
+++ Chapter4/mathObjects.js
@@ -1,5 +1,5 @@
                                                                                 //  document.write(`'${}'`);
-                                                                                //  <>  <br>
+                                                                                //  <b>  <br>
                                                                                 //  *filter()
                                                                                 //  style='color:#FF0000'
 //math Objects 
@@ -28,6 +28,72 @@ document.write(`el numero max es : '${numMax}'<br><br>`);
 
 //random() *provides random number
 document.write(`<b style='color:#FF0000'>random() *provides random number</b><br>`);
-numRandoom = Math.random(); 
-document.write(`Random number : '${numRandoom}'<br><br>`);
+numRandoom = Math.random()*100; 
+document.write(`Random number zero to one hundred : '${numRandoom}'<br>`);
+numRandoom = numRandoom.toString();//makes random number whole and also makes it show 
+num = numRandoom[0] + numRandoom[1]; 
+if (numRandoom[1] == '.'){
+    numRandoom[1] = numRandoom[0];
+};
+document.write(`adding <b style='color:#FF0000'>toString()</b> is how we can get the random number on here : <b>'${num}'</b> but <b style='color:#FF0000'> toString() </b> is  also making number <b>whole</b><br><br><br>`);
+//
+//document.write(`'${}'`);
+
+
+//round() **en vez de hacer lo de arriba
+document.write(`<b style='color:#FF0000'> *Round() **en vez de hacer lo de arriba but provides round number </b><br>`);
+let numeroRound = Math.random()*100; //from (anything) to 100
+numeroRound = Math.round(numeroRound);//el  numero es igual a el numero redondeado
+if (numeroRound[1] == '.'){
+    numeroRound[1] = numeroRound[0];
+};
+document.write(`round number zero to one hundred : '${numeroRound}'<br><br>`); 
+
+
+//floor() **also does kinda the same but ROUNDS DOWN
+document.write(`<b style='color:#FF0000'> *Floor() **also does kinda the same but ROUNDS DOWN the number </b><br>`);
+let numeroFloor = Math.random()*99;//to one hundred but to prevent zero from showing change to 99 AND 
+numeroFloor = Math.floor(numeroFloor + 1);//AND add one here
+document.write(`round down number from zero to one hundred(99+1) : '${numeroFloor}'<br><br>`);
+// to test one and onehundred show
+document.write(`<b> *Ran function loop 1K times </b><br>`);
+for (var i = 0; i<1; i++){
+    let numLoop = Math.random()*100; 
+    numLoop = Math.floor(numLoop + 1); 
+
+    document.write(`numero : '${numLoop}'<br>`);
+}; 
+document.write(`<br><br>`);
+
+//fround() *returns la representancion flotante de precicion mas simple close to the number
+document.write(`<b style='color:#FF0000'> fround() *returns la representancion flotante de precicion mas simple (4bytes) close to the number</b> <b>(it just shows a simpler number)
+</b><br>`);
+let numeroFround = Math.random()*100;
+numeroFround = Math.fround(numeroFround + 1); 
+document.write(`fround number 1 to 100`);
+document.write(`this is numero fround : '${numeroFround}' a four byte 'simpler' number<br><br><br>`);
+//fround number 23.34168815612793
+
+
+//trunc() 
+document.write(`<b style='color:#FF0000'> trunc()     *devuelve la parte entera del numero (removes fractions) </b><br>`);
+let numTrunc = Math.random()*100;
+numTrunc = Math.trunc(numTrunc); 
+document.write(`the fractions are removed : '${numTrunc}'<br><br><br>`);
+
+let numPi = Math.PI; 
+document.write(`<b style='color:#FF0000'>*El PI number : '${numPi}' </b><br>`);
+numPi = Math.fround(numPi); 
+document.write(`<b style='color:#FF0000'>*El PI number :_${numPi} _'fround version'</b><br><br><br>`);
+
+
+//SQRT1_2  *
+let numSQRT1_2 = Math.SQRT1_2; 
+document.write(`<b style='color:#FF0000'> *La representacion flotante mas cercana : '${numSQRT1_2}'</b><br>`);
+
+
+//SQRT2
+let 
+
+
 
